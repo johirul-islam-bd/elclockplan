4ee60c75bea0739bf295066bfca5f58f5e3256eb
johirul-islam-bd <2johirul@gmail.com>
2026-09-23 15:57:21 +0600

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.EWO_CONFIG = {
 
   /* Azure portal > App registrations > "Dashboard" > Overview >
      Application (client) ID. Required. */
-  CLIENT_ID: "",
+  CLIENT_ID: "4e8f9920-a497-4a73-ad56-f08e85d659e0",
 
   /* Epyllion Group's Directory (tenant) ID. Only Epyllion accounts can sign in. */
   TENANT_ID: "09438fa4-a67e-4666-a9c2-fcc1c2252472",
